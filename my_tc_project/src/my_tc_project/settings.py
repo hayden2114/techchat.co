4f087d8592a1526ab460fb5b83c93c0a9dfa90da
haydeng21 <hayden@coilapp.com>
2016-09-14 22:23:16 -0500

    put settings back to normal

--- my_tc_project/src/my_tc_project/settings.py
+++ my_tc_project/src/my_tc_project/settings.py
@@ -69,21 +69,19 @@ TEMPLATES = [
     },
 ]
 
-#WSGI_APPLICATION = 'my_tc_project.wsgi.application'
-WSGI_APPLICATION = 'techchatblog.wsgi'
+WSGI_APPLICATION = 'my_tc_project.wsgi.application'
+
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#} 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+} 
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation
